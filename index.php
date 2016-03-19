f85a0761c8af3e258ac9607c09d8985f14a054bd
gilsalgon <gilsalgon76@hotmail.com>
2016-03-19 13:52:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			ampersand pc soluciones
+		</title>
+		<link rel="stylesheet" href="estilo.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<h1 class="titulo">BIENVENIDOS AMPERSAND PC SOLUCIONES</h1>
+		<nav>
+			<ul>
+				<li><a href="http://google.com/"><i class="fa fa-home"></i> Inicio</a></li>
+				<li>Cotizar</li>
+				<li>Quienes somos</li>
+				<li>Contacto</li>
+			</ul>
+		</nav>
+	</body>
+</html>
