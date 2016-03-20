25ceed769687c382a61eb74e8541cd45ee3282e6
gilsalgon <gilsalgon76@hotmail.com>
2016-03-19 20:03:42 -0600

    Cambio a mayuscula
    Cambio a mayuscula index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		<title>
-			ampersand pc soluciones
+			Ampersand PC Soluciones
 		</title>
 		<link rel="stylesheet" href="estilo.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
